32237cf9a4f44941f637b872659a0bc48d480979
Anson Jablinski <a@ansonj.org>
2017-07-05 22:51:16 -0500

    Escape double quotes in pages.js generation

--- pages/gen.rb
+++ pages/gen.rb
@@ -7,7 +7,7 @@ raise 'You\'re probably not in the right directory' unless input_files.count > 5
 output_file = File.open('../pages.js', 'w')
 
 input_files.each do |f|
-  lines = File.readlines("#{f}.html").map(&:chomp).join("\\\n")
+  lines = File.readlines("#{f}.html").map(&:chomp).join("\\\n").gsub('"', '\"')
   output_file.puts "var page_#{f} = \"#{lines}\";"
   output_file.puts
 end
