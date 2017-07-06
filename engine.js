e2298c96cd1362946c17e255cd0de31952fa7f54
Anson Jablinski <a@ansonj.org>
2017-07-05 22:41:39 -0500

    Improve next button show/hide

--- engine.js
+++ engine.js
@@ -1,6 +1,8 @@
 var currentPage = 0;
 
 function nav_nextPage() {
+    // do validation on inputs here, if needed
+
     currentPage += 1;
 
     nav_loadPage(currentPage);
@@ -10,6 +12,14 @@ function nav_loadPage(pageNumber) {
     document.getElementById('page-contents').innerHTML = pages[pageNumber];
 
     if (pageNumber == pages.length - 1) {
-        document.getElementById('nav-link-right').innerHTML = '';
+        nav_hideNextButton();
     }
+}
+
+function nav_hideNextButton() {
+    document.getElementById('nav-link-right').innerHTML = '';
+}
+
+function nav_showNextButton() {
+    document.getElementById('nav-link-right').innerHTML = 'Next page >>';
 }
