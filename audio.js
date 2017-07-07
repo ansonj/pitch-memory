b50e872d0e24e5b7aa5eaaf7a9431f36e3465225
Anson Jablinski <a@ansonj.org>
2017-07-06 21:20:25 -0500

    Get audio working
    - Everything went better than expected.

--- audio.js
+++ audio.js
@@ -1,10 +1,18 @@
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+var oscillator = audioContext.createOscillator();
+oscillator.type = 'sine';
+oscillator.connect(audioContext.destination);
+
 function audio_start(hertz) {
+    audio_change(hertz);
+    oscillator.start();
 }
 
 function audio_change(hertz) {
-
+    oscillator.frequency.value = hertz;
 }
 
 function audio_stop() {
-
+    oscillator.stop();
 }
