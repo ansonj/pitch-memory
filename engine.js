49f46b34af38548051bf845b2440506435c01225
Anson Jablinski <a@ansonj.org>
2017-07-06 23:00:11 -0500

    Draft demographic page and validation

--- engine.js
+++ engine.js
@@ -2,6 +2,12 @@ var startPage = 0;
 var currentPage = startPage - 1;
 
 var resp_hz1;
+var resp_demo_age;
+var resp_demo_gender;
+var resp_demo_instrument;
+var resp_demo_vocal_range;
+var resp_demo_how_often;
+var resp_demo_last_played;
 
 function nav_nextPage() {
     currentPage += 1;
@@ -26,4 +32,45 @@ function slider_done(number) {
     // temporary
     alert("You picked " + resp_hz1 + "Hz");
     nav_nextPage();
+}
+
+function demo_validate() {
+    var allFields = [
+        "age",
+        "gender",
+        "instrument",
+        "vocal-range",
+        "how-often",
+        "last-played"
+    ];
+    var badElements = [];
+    function extractValue(field) {
+        return document.getElementById(field).value.trim();
+    }
+
+    for (var i = 0; i < allFields.length; i++) {
+        var field = "demo-" + allFields[i];
+        var value = extractValue(field);
+        if (value == "") {
+            badElements.push(document.getElementById(field));
+        }
+    }
+    if (badElements.length == 0) {
+        resp_demo_age = extractValue("demo-age");
+        resp_demo_gender = extractValue("demo-gender");
+        resp_demo_instrument = extractValue("demo-instrument");
+        resp_demo_vocal_range = extractValue("demo-vocal-range");
+        resp_demo_how_often = extractValue("demo-how-often");
+        resp_demo_last_played = extractValue("demo-last-played");
+        nav_nextPage();
+    } else {
+        badElements[0].focus();
+        function changeColor(elements, color) {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.backgroundColor = color;
+            }
+        }
+        changeColor(badElements, "red");
+        setTimeout(function(){changeColor(badElements, "white")}, 1000);
+    }
 }
