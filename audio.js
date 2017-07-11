fca87021c97856bd19def35cd5870eb89f6c459f
Anson Jablinski <a@ansonj.org>
2017-07-10 22:36:22 -0500

    Fix audio not working after first time

--- audio.js
+++ audio.js
@@ -1,11 +1,14 @@
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-var oscillator = audioContext.createOscillator();
-oscillator.type = 'sine';
-oscillator.connect(audioContext.destination);
+var oscillator;
 
 function audio_start(hertz) {
+    oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.connect(audioContext.destination);
+
     audio_change(hertz);
+
     oscillator.start();
 }
 
