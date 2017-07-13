7314743a3d6a82f76ac8c5fb176b834eeeed6eb9
Anson Jablinski <a@ansonj.org>
2017-07-12 23:06:05 -0500

    Revise listening test instructions

--- pages.js
+++ pages.js
@@ -127,7 +127,7 @@ var page_07 = "<div class=\"page-text\">\
         <br><input id=\"demo-other-instruments\" class=\"demo-input\" size=\"40\" />\
     </p>\
     <p>\
-        The next and final part of the study is a brief listening test, consisting of eight isolated tones for you to identify. A tone will play for a moment, and then you will have five seconds to click the note name (such as C, C#/Db, D, etc.) that best fits the tone. Don't be afraid to make a best guess if you are unsure&mdash;it's better to guess than not to respond at all.\
+        The next and final part of the study is a brief listening test, consisting of eight different, isolated tones for you to identify. One tone will play for a moment, and then you will have five seconds to click the note name (such as C, C#/Db, D, etc.) that best fits that tone. Don't be afraid to make a best guess if you are unsure&mdash;it's better to guess than not to respond at all.\
     </p>\
     <p>\
         When you're ready to begin the listening test, click to continue. The buttons will appear, and the first tone will begin playing automatically.\
