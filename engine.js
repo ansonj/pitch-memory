08706b8e0891bacec8a8d732d87ef20af4548c26
Anson Jablinski <a@ansonj.org>
2017-07-25 23:00:13 -0500

    Add debug mode

--- engine.js
+++ engine.js
@@ -426,3 +426,81 @@ function frequency_for_pitch(pitch) {
     pitchTable["G5"] = 783.99;
     return pitchTable[pitch];
 }
+
+var debug_playing = false;
+var debug_index = 0;
+var debug_pitches = [
+    "F2",
+    "F#2",
+    "G2",
+    "G#2",
+    "A2",
+    "A#2",
+    "B2",
+    // "C3", // not including C3
+    "C#3",
+    "D3",
+    "D#3",
+    "E3",
+    "F3",
+    "F#3",
+    "G3",
+    "G#3",
+    "A3",
+    "A#3",
+    "B3",
+    // "C4", // not including C4
+    "C#4",
+    "D4",
+    "D#4",
+    "E4",
+    "F4",
+    "F#4",
+    "G4",
+    "G#4",
+    "A4",
+    "A#4",
+    "B4",
+    // "C5", // not including C5
+    "C#5",
+    "D5",
+    "D#5",
+    "E5",
+    "F5",
+    "F#5",
+];
+
+function debug_start() {
+    if (debug_playing) {
+        audio_stop();
+    } else {
+        audio_start(0);
+    }
+    debug_playing = !debug_playing;
+    debug_update_pitch();
+}
+
+function debug_step_down() {
+    if (debug_index > 0) {
+        debug_index -= 1;
+    }
+    debug_update_pitch();
+}
+
+function debug_step_up() {
+    if (debug_index < debug_pitches.length - 1) {
+        debug_index += 1;
+    }
+    debug_update_pitch();
+}
+
+function debug_update_pitch() {
+    var pitch = debug_pitches[debug_index];
+    var freq = frequency_for_pitch(pitch);
+    var message = "pitch " + debug_index + "/" + debug_pitches.length + " is " + pitch + " @ " + freq;
+    if (!debug_playing) {
+        message += " (paused)";
+    }
+    document.getElementById('debug-current').innerHTML = message;
+    audio_change(freq);
+}
